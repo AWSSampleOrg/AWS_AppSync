d5b97fb4105b73f2dfb727fbc16f6a05d8cfa748
Yo-mah-Ya <shanzhongziye@gmail.com>
2024-05-18 20:16:15 -0400

    added JS resolver

---
+++ JS/DynamoDB/test.js
@@ -0,0 +1,47 @@
+const APP_SYNC_ENDPOINT_URL = "";
+const API_KEY = "";
+
+/**
+ *
+ * @param {string} query
+ * @param {Record<string, unknown> | undefined} variables
+ * @returns
+ */
+const execute = async (query, variables) => {
+  const response = await fetch(APP_SYNC_ENDPOINT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/graphql",
+      "x-api-key": API_KEY,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  return response.ok ? response.json() : response.statusText;
+};
+
+(async () => {
+  console.log(
+    await execute(`
+      mutation put{
+        put_item( id: "1",name: "Test",age: 100 ){
+          id,
+          name,
+          age
+        }
+      }`)
+  );
+  console.log(
+    await execute(`
+    query get_item{
+      get_item(id: "1"){
+        id,
+        name,
+        age
+      }
+    } 
+    `)
+  );
+})().catch(console.error);
---
+++ JS/SQS/test.js
@@ -0,0 +1,44 @@
+const APP_SYNC_ENDPOINT_URL = "";
+const API_KEY = "";
+
+/**
+ *
+ * @param {string} query
+ * @param {Record<string, unknown> | undefined} variables
+ * @returns
+ */
+const execute = async (query, variables) => {
+  const response = await fetch(APP_SYNC_ENDPOINT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/graphql",
+      "x-api-key": API_KEY,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  return response.ok ? response.json() : response.statusText;
+};
+
+(async () => {
+  console.log(
+    await execute(
+      `
+    mutation SendMessage($data: String) {
+      sendMessage(data: $data) {
+        MD5OfMessageBody
+        MessageId
+      }
+    }`,
+      { data: "body" }
+    )
+  );
+  console.log(
+    await execute(`
+    query ReceiveMessage {
+      receiveMessage
+    }`)
+  );
+})().catch(console.error);
